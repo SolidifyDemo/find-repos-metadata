9fc9f81fbf11fc8bdc98de930ac185f6212c2fde
Mattias Bälter <mattias.balter@solidify.dev>
2023-06-26 14:44:13 +0200

    changed how API is called

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-import { Octokit } from "@actions/core";
-
 const core = require('@actions/core')
 const github = require('@actions/github')
 /* import fs from 'fs' */
+const main = async () => {
+    try {
+        const token = core.getInput('token')
+        const visibility = core.getInput('visibility')
+    // Octokit.js
+    // https://github.com/octokit/core.js#readme
+    const octokit = new github.getOctokit(token)
 
-try {
-    const token = core.getInput('token')
-    const visibility = core.getInput('visibility')
-// Octokit.js
-// https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-    auth: token,
-})
+    // Enterprise cloud
+    const { data: orgs } = await octokit.rest.orgs.listForAuthenticatedUser();
 
-// Enterprise cloud
- const { data: orgs } = await octokit.request('GET /user/orgs',{
-    headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
+    orgs.forEach(async org => {
+        getRepos(org, visibility)
+    })
+
+    //Remember to check how many repos there are and if there are more than 100, use pagination
+    async function getRepos(org, visibility) {
+        const { data: repos } = await octokit.rest.repos.listForOrg({
+            org: org.login,
+            per_page: 100,
+            type: visibility,
+        })
+        var data = repos.map(repo => {
+            return {
+                repository: repo.name,
+                organization: org.login,
+                visibility: repo.visibility,
+            }
+        })
+        core.setOutput('repos', JSON.stringify(data))
+
+        return data
     }
-})
-
-orgs.forEach(async org => {
-    getRepos(org, visibility)
-})
-
-//Remember to check how many repos there are and if there are more than 100, use pagination
-async function getRepos(org, visibility) {
-    const { data: repos } = await octokit.request('GET /orgs/{org}/repos', {
-        org: org.login,
-        per_page: 100,
-        type: visibility,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        },
-    })
-    var data = repos.map(repo => {
-        return {
-            repository: repo.name,
-            organization: org.login,
-            visibility: repo.visibility,
-        }
-    })
-    core.setOutput('repos', JSON.stringify(data))
-
-    return data
-}
-} catch (error) {
-    core.setFailed(error.message)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
