3ac3c1d59e8fcc8ee00f0d9fac08e4586d33a9ee
Mattias Bälter <mattias.balter@solidify.dev>
2023-06-26 16:00:41 +0200

    Should return all repos of all orgs now

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-/* import fs from 'fs' */
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 const main = async () => {
-    try {
-        const token = core.getInput('token')
-        const visibility = core.getInput('visibility')
-    // Octokit.js
-    // https://github.com/octokit/core.js#readme
-    const octokit = new github.getOctokit(token)
+  try {
+    const token = core.getInput("token");
+    const visibility = core.getInput("visibility");
 
-    // Enterprise cloud
+    const octokit = new github.getOctokit(token);
+
     const { data: orgs } = await octokit.rest.orgs.listForAuthenticatedUser();
-
-    orgs.forEach(async org => {
-        getRepos(org, visibility)
-    })
-
+    var allRepos = [];
+    var count = 0;
+    orgs.forEach(async (org) => {
+      await getRepos(org, visibility, allRepos);
+    });
     //Remember to check how many repos there are and if there are more than 100, use pagination
-    async function getRepos(org, visibility) {
-        const { data: repos } = await octokit.rest.repos.listForOrg({
-            org: org.login,
-            per_page: 100,
-            type: visibility,
-        })
-        var data = repos.map(repo => {
-            return {
-                repository: repo.name,
-                organization: org.login,
-                visibility: repo.visibility,
-            }
-        })
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
-        core.setOutput("repos", JSON.stringify(data))
-
-        return data
+    async function getRepos(org, visibility, allRepos, nrOrgs = orgs.length) {
+      const { data: repos } = await octokit.rest.repos.listForOrg({
+        org: org.login,
+        per_page: 100,
+        type: visibility,
+      });
+      var data = repos.map((repo) => {
+        return {
+          repository: repo.name,
+          organization: org.login,
+          visibility: repo.visibility,
+        };
+      });
+      allRepos = allRepos.concat(data);
+      count++;
+      if (count == nrOrgs) {
+        core.setOutput("repos", JSON.stringify(data));
+      }
     }
-    } catch (error) {
-        core.setFailed(error.message)
-    }
-}
-main()
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+main();
